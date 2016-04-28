b430885a900b2d922f0e5aa564b01104eb8f8192
Matt Thompson <mthompson@hexwave.com>
2016-04-28 17:38:04 -0500

    add example

---
+++ firmware/examples/Sensors.ino
@@ -0,0 +1,23 @@
+
+Adafruit_LSM303_Accel_Unified accel;
+Adafruit_LSM303_Mag_Unified mag;
+
+void setup() {
+    Serial.begin(115200);
+    
+    accel.begin();
+    mag.begin();
+}
+
+void loop() {
+    sensors_event_t event;
+    
+    mag.getEvent(&event);
+    
+    Serial.print("Mag X: ");
+    Serial.println(event.magnetic.x);
+    
+    accel.getEvent(&event);
+    Serial.print("Accel X: ");
+    Serial.println(event.acceleration.x);
+}
